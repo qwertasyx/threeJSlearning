2bfac2b99f31977d3d4422c5e359fb809925ee38
qwertasyx <qwertasyx@gmail.com>
2018-08-12 22:03:50 +0200

    + chained animations

--- js/interaction.js
+++ js/interaction.js
@@ -19,13 +19,49 @@ stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild( stats.dom );
 
 // tweening
-// TWEEN.update();
+var t1 = new TWEEN.Tween(cube.position);
+var t2 = new TWEEN.Tween(cube.position);
+var t3 = new TWEEN.Tween(cube.position);
+var t4 = new TWEEN.Tween(cube.position);
+t1.onUpdate(function(object) {
+	console.log(object.x);
+});
+t1.to({ x: -200 ,y: -200 }, 1000);
+t2.to({ x: -200 ,y: 200 }, 1000);
+t3.to({ x: 200  ,y: 200 }, 1000);
+t4.to({ x: 200  ,y: -200 }, 1000);
+
+var t5 = new TWEEN.Tween(cube.rotation);
+var t6 = new TWEEN.Tween(cube.rotation);
+t5.to({ z: 2 }, 1000);
+t6.to({ z: 0 }, 1000);
+
+var t7 = new TWEEN.Tween(cube.scale);
+var t8 = new TWEEN.Tween(cube.scale);
+t7.to({ x: 10  ,y: 10 }, 1000);
+t8.to({ x: 1  ,y: 1 }, 1000);
+
+t1.chain(t2,t5,t7)
+t2.chain(t3,t6,t8)
+t3.chain(t4)
+
+var m1 = new TWEEN.Tween(material);
+var m2 = new TWEEN.Tween(material);
+m1.to({ color:{r:1}} , 1000);
+m2.to({ color:{r:0}}, 1000);
 
 // rdy
 $( document ).ready(function() {
   console.log( "ready!" );
-  $('#test').on('click',function(e){
-    //do something
+  $('#start').on('click',function(e){
+    t1.start()
+  })
+  $('#stop').on('click',function(e){
+    var tweens = TWEEN.getAll()
+    tweens.forEach(function(t) {
+      t.stop()
+    });
+    // t1.stop()
   })
 });
 // load
@@ -46,11 +82,11 @@ $( window ).on( "load", function() {
     f2.add(cube.scale, 'x', 0, 10);
     f2.add(cube.scale, 'y', 0, 10);
   f2.open();
-  var fr = gui.addFolder('Rotation');
-    fr.add(cube.rotation, 'x', -10, 10);
-    fr.add(cube.rotation, 'y', -10, 10);
-    fr.add(cube.rotation, 'z', -10, 10);
-  f4.open();
+  var r = gui.addFolder('Rotation');
+    r.add(cube.rotation, 'x', -10, 10)
+    r.add(cube.rotation, 'y', -10, 10)
+    r.add(cube.rotation, 'z', -10, 10)
+    r.open()
 
   $('#test').on('click',function(e){
     gui.toggleHide()
@@ -61,14 +97,6 @@ function animate() {
   stats.begin();
   requestAnimationFrame( animate );
   TWEEN.update();
-  if (cube.position.y >= 250){
-    cube.position.y = -250
-  } else {
-    cube.translateY(cube.vx)
-  }
-  
-  // // cube.rotation.x += 0.01;
-  // cube.rotation.z += 0.02;
   renderer.render( scene, camera );
   stats.end();
 }
